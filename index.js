2128f0c1600c3222ae73e799a6667809ec957278
DiegoLopesLima <web.diego.lima@gmail.com>
2018-11-13 14:02:48 -0200

    Setting Express, CORS, static path and default route.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello world!');
+const
+
+	express = require('express'),
+
+	app = express(),
+
+	port = 7000,
+
+	cors = require('cors');
+
+// Make public folder an static directory.
+app.use(express.static('public'));
+
+// Make the application CORS enabled for all origins.
+app.use(cors());
+
+// Hello world!
+app.get('/', (request, response) => response.json({ messages: [ { body: 'Hello world!' } ] }));
+
+// Run the server on port ${port}.
+app.listen(port, () => console.log(`Listening on port ${port}.`));
